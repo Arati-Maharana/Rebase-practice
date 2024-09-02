08e7772920ab904067135d1ece5a52fed5eadbcf
Arati-Maharana <aratimaharana95@gmail.com>
2024-09-02 23:55:43 +0530

    Arati's code added

--- main.tf
+++ main.tf
@@ -7,3 +7,7 @@ resource "azurerm_resource_group" "rgblock2" {
   name     = "2nd RG"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "Aratiblock" {
+  name     = "3rd-Arati-RG"
+  location = "West Europe"
+}
